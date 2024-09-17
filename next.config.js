eb4275d7573fddc678723bc8cf39c9f6dd8ca5f2
Bruno Gomes <brunog.vix@gmail.com>
2024-09-17 19:17:34 -0300

    removing the .env in config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    env: {
-      SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-      SHOPIFY_ACCESS_TOKEN: process.env.SHOPIFY_ACCESS_TOKEN,
-    },
     images: {
       domains: ['cdn.shopify.com'], 
     },
